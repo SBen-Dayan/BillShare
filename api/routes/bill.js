144d6a6973ebdb5a4d358f4b929ba5df5cbf5233
SBen-Dayan <shanibdayan@gmail.com>
2024-06-09 18:00:49 -0400

    Added backend routes

--- api/routes/bill.js
+++ api/routes/bill.js
@@ -1,6 +1,20 @@
 const router = require('express').Router();
-const db = require('../data');
+const data = require('../data');
 
+router.get('/getAll', async (_, res) => {
+    res.json(await data.getBillsAndParticipantCount());
+})
 
+router.get('/getBillAndParticipants/:billId', async (req, res) => {
+    const bill = await data.getBill(req.params.billId);
+    const participants = await data.getParticipantsForBill(req.params.billId);
+    res.json({bill, participants});
+})
+
+router.post('/addWithParticipants', async (req, res) => {
+    const billId = await data.addBill(req.body.amount, new Date());
+    await data.addBillParticipants(billId, req.body.participantIds);
+    res.json({});
+})
 
 module.exports = router;
--- api/routes/participant.js
+++ api/routes/participant.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
-const db = require('../data');
+const data = require('../data');
 
+router.get('/getAll', async (_, res) => {
+    res.json(await data.getParticipants());
+})
 
+router.post('/add', async (req, res) => {
+    await data.addParticipant(req.body);
+    res.json({});
+})
 
 module.exports = router;
